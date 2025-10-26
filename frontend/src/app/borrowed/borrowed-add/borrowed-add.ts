c3d1837617747981739916ec8be14a92a82a694c
Hakuka <29175842+Hakuka@users.noreply.github.com>
2025-10-26 16:04:21 +0100

    added control component, borrowed-add move to

--- frontend/src/app/borrowed/borrowed-add/borrowed-add.ts
+++ frontend/src/app/borrowed/borrowed-add/borrowed-add.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { ControlComponent } from '../../shared/control/control';
 import { CopiesService } from '../../shared/services/copies.service';
 import { GamesService } from '../../shared/services/games.service';
 @Component({
   selector: 'borrowed-add',
-  imports: [FormsModule],
+  imports: [FormsModule, ControlComponent],
   templateUrl: './borrowed-add.html',
   styleUrl: './borrowed-add.css',
 })

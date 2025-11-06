f0abf77836bcc2374760ca8e1c7942cdaf323af4
Hakuka <29175842+Hakuka@users.noreply.github.com>
2025-11-06 19:51:33 +0100

    moved button to attSelector

--- frontend/src/app/shared/button/button.ts
+++ frontend/src/app/shared/button/button.ts
@@ -3,10 +3,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 type ButtonType = 'button' | 'submit';
 type Variant = 'primary' | 'secondary';
 @Component({
-  selector: 'app-button',
+  selector: 'button[appButton]',
   imports: [],
   templateUrl: './button.html',
   styleUrl: './button.css',
+  host: {
+    '[attr.type]': 'buttonType',
+    '[disabled]': 'disabled',
+    '[class]': "'btn btn-' + variant",
+    '(click)': 'onButtonClick()',
+  },
 })
 export class ButtonComponent {
   @Input({ required: true }) buttonType!: ButtonType;
